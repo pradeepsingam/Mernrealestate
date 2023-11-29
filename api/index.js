23efd9b61549ea217fdd75998bebf6a3e69d453b
pradeep singam <ipradeep.here@gmail.com>
2023-11-29 08:56:44 +0530

    Mongo db connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to DB");
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000 !!')
+    console.log('Server is running on port 3000')
 })
