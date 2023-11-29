1b7d9d1a9549328bcf88d3ec3711e738ba7402ce
pradeep singam <ipradeep.here@gmail.com>
2023-11-29 11:43:11 +0530

    Usermodel created

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mangoose from 'mangoose';
+import mongoose from 'mongoose';
+const { Schema } = mangoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    password: {
+        type: String,
+        required: true,
+    }, 
+    }, {timestamps: true})
+
+    const User = mongoose.model('User',userSchema);
+
+    export default User
